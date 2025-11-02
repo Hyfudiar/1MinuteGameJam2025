220e48bc6bcfedf89f13e194c982af69cb320f2b
Hyfudiar <hyfudiarmusic@gmail.com>
2025-11-01 21:13:58 -0400

    before leaving family

---
+++ Scenes/hourglass.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+enum COLORS {R, B, Y}
+
+@export var item_color : COLORS
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if item_color == COLORS.R:
+		modulate = Color.hex(0xff6772ff)
+	if item_color == COLORS.B:
+		modulate = Color.hex(0x54a3ffff)
+	if item_color == COLORS.Y:
+		modulate = Color.hex(0xfff660ff)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
