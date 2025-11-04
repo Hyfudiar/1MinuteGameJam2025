9e45261b1371d945c2956d56bfe071bb292fdd9c
sun-void <84147748+sun-void@users.noreply.github.com>
2025-11-03 23:41:12 -0600

    hourglass ui

---
+++ Scenes/draining_sand.gd
@@ -0,0 +1,17 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play("SandDrainingStart")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+
+func _on_animation_finished():
+	
+	play("SandDraining")
+	pass # Replace with function body.
