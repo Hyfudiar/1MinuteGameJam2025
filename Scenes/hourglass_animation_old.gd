af6e6c608a2f0ac624234400069a80e9cacf5c56
sun-void <84147748+sun-void@users.noreply.github.com>
2025-11-08 21:49:39 -0600

    bridges and hourglass fix

--- Scenes/hourglass_animation_old.gd
+++ Scenes/hourglass_animation_old.gd
@@ -12,6 +12,14 @@ var hourglass_yellow = Color.hex(0xfff660ff)
 func _ready():
 	#set_visible(false)
 	pass
+
+func hourglass_fadeout():
+	var fadeout = get_tree().create_tween()
+	fadeout.tween_property($Node2D/hourglass_body, "self_modulate:a", 0.0, 0.3)
+	
+func hourglass_fadein():
+	var fadein = get_tree().create_tween()
+	fadein.tween_property($Node2D/hourglass_body, "self_modulate:a", 1.0, 0.3)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):	
@@ -41,8 +49,61 @@ func _on_hourglass_yellow_body_entered(body):
 	hourglass_ui_color = hourglass_yellow
 	return hourglass_ui_color
 	print("Hourglass Yellow Received")
-	
+
+
 	#hourglass_ui_color = hourglass_yellow
 	#set_modulate(Color.hex(0xfff660ff))
 	#set_visible(true)
 	#pass
+	
+
+func _on_timer_30_timeout():
+	hourglass_fadeout()
+
+
+func _on_timer_31_timeout():
+	hourglass_fadein()
+
+
+func _on_timer_60_timeout():
+	hourglass_fadeout()
+	
+
+func _on_timer_61_timeout():
+	hourglass_fadein()
+
+
+func _on_timer_62_timeout():
+	hourglass_fadeout()
+	
+	
+func _on_timer_63_timeout():
+	hourglass_fadein()
+	
+
+
+func _on_timer_80_timeout():
+	hourglass_fadeout()
+	
+
+func _on_timer_81_timeout():
+	hourglass_fadein()	
+
+func _on_timer_82_timeout():
+	hourglass_fadeout()
+
+
+func _on_timer_83_timeout():
+	hourglass_fadein()
+	
+
+func _on_timer_84_timeout():
+	hourglass_fadeout()
+
+
+func _on_timer_85_timeout():
+	hourglass_fadein()
+
+
+func _on_timer_89_timeout():
+	hourglass_fadeout()
